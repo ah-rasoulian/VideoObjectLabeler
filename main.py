56684ed2e90d2f0a17cdaba163bb90f307babb78
Amirhossein <ah.rasoulian@gmail.com>
2021-10-09 11:18:56 +0330

    Added a simple menu bar and toolbar.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from tkinter import *
-from tkinter import ttk
 import threading
 import queue
 import time
+from PIL import Image, ImageTk
 
 
 class GUI(Tk):
@@ -10,13 +10,43 @@ class GUI(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.queue = queue.Queue()
-        self.listbox = Listbox(self, width=100, height=5)
-        self.progressbar = ttk.Progressbar(self, orient='horizontal',
-                                           length=300, mode='determinate')
-        self.button = Button(self, text="Start", command=self.spawnThread)
-        self.listbox.pack(padx=10, pady=10)
-        self.progressbar.pack(padx=10, pady=10)
-        self.button.pack(padx=10, pady=10)
+        self.thread = None
+
+        # determine the basic arguments of main window
+        self.title('Object Labeler')
+
+        ################################################################################################################
+        # add menu bar
+        self.menu_bar = Menu(self)
+        self.config(menu=self.menu_bar)
+        # add a file sub menu
+        self.file_menu = Menu(self.menu_bar, tearoff=False)
+        self.file_menu.add_command(label='Exit', command=self.destroy)
+        self.menu_bar.add_cascade(label='File', menu=self.file_menu)
+        # add a help sub menu
+        self.help_menu = Menu(self.menu_bar, tearoff=False)
+        self.help_menu.add_command(label='About...')
+        self.menu_bar.add_cascade(label='Help', menu=self.help_menu)
+        ################################################################################################################
+        # add toolbar
+        self.toolbar = Frame(self, bd=1, relief=RAISED)
+        self.toolbar.pack(side=TOP, fill=X)
+
+        self.add_icon = ImageTk.PhotoImage(Image.open('Icons/add.png'))
+        self.add_button = Button(self.toolbar, image=self.add_icon, relief=FLAT)
+        self.add_button.pack(side=LEFT, padx=2, pady=2)
+
+        self.done_icon = ImageTk.PhotoImage(Image.open('Icons/done.jpg'))
+        self.done_button = Button(self.toolbar, image=self.done_icon, relief=FLAT)
+        self.done_button.pack(side=LEFT, padx=2, pady=2)
+
+        self.save_icon = ImageTk.PhotoImage(Image.open('Icons/save.jpg'))
+        self.save_button = Button(self.toolbar, image=self.save_icon, relief=FLAT)
+        self.save_button.pack(side=LEFT, padx=2, pady=2)
+        ################################################################################################################
+        # add main frame
+        self.main_frame = Frame(self, width=400, height=300)
+        self.main_frame.pack(fill=X)
 
     def spawnThread(self):
         self.button.config(state="disabled")
