d86cca4e3018a5d849aba6fc40f1fe563dbc9ffd
Amirhossein <ah.rasoulian@gmail.com>
2021-10-10 09:57:35 +0330

    Added change frame buttons.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
 import threading
-import queue
 import time
 from PIL import Image, ImageTk
 import cv2
@@ -13,9 +12,10 @@ class GUI(Tk):
 
     def __init__(self):
         Tk.__init__(self)
-        self.queue = queue.Queue()
+        self.frames_list = list()
         self.thread = None
         self.main_frame_image = None
+        self.current_frame_index = 0
 
         # determine the basic arguments of main window
         self.title('Object Labeler')
@@ -61,12 +61,30 @@ class GUI(Tk):
         self.main_frame.grid(row=1)
 
         ################################################################################################################
+        # add buttons pane
+        self.buttons_frame = Frame(self)
+
+        self.previous_icon = ImageTk.PhotoImage(Image.open('Icons/previous.jpg'))
+        self.previous_button = Button(self.buttons_frame, image=self.previous_icon, relief=FLAT)
+        self.previous_button.pack(side=LEFT, padx=2, pady=2)
+        self.previous_button.bind('<Enter>', lambda event: self.on_hover('previous'))
+        self.previous_button.bind('<Leave>', lambda event: self.on_leave())
+        self.previous_button.bind('<Button-1>', lambda event: self.change_picture('previous'))
+
+        self.next_icon = ImageTk.PhotoImage(Image.open('Icons/next.png'))
+        self.next_button = Button(self.buttons_frame, image=self.next_icon, relief=FLAT)
+        self.next_button.pack(side=LEFT, padx=2, pady=2)
+        self.next_button.bind('<Enter>', lambda event: self.on_hover('next'))
+        self.next_button.bind('<Leave>', lambda event: self.on_leave())
+        self.next_button.bind('<Button-1>', lambda event: self.change_picture('next'))
+        ################################################################################################################
         # add status bar
         self.status_bar = Frame(self)
         self.status_bar.grid(row=3, sticky='w')
 
         self.status = Label(self.status_bar)
         self.status.pack(side=LEFT)
+        ################################################################################################################
 
     def on_hover(self, text):
         self.status.configure(text=text)
@@ -74,8 +92,36 @@ class GUI(Tk):
     def on_leave(self):
         self.status.configure(text='')
 
+    def change_picture(self, direction):
+        if direction == 'next':
+            if self.current_frame_index < len(self.frames_list):
+                self.current_frame_index += 1
+            else:
+                return
+        elif direction == 'previous':
+            if self.current_frame_index > 0:
+                self.current_frame_index -= 1
+            else:
+                return
+
+        if self.current_frame_index == 0:
+            self.previous_button['state'] = DISABLED
+        elif self.previous_button['state'] == DISABLED:
+            self.previous_button['state'] = NORMAL
+
+        if self.current_frame_index == len(self.frames_list) - 1:
+            self.next_button['state'] = DISABLED
+        elif self.next_button['state'] == DISABLED:
+            self.next_button['state'] = NORMAL
+
+        image = cv2.cvtColor(self.frames_list[self.current_frame_index], cv2.COLOR_BGR2RGB)
+        self.main_frame.configure(height=image.shape[0], width=image.shape[1])
+        self.main_frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+        self.main_frame.configure(image=self.main_frame_image)
+        print(self.current_frame_index)
+
     def spawn_file_reader_thread(self):
-        self.thread = ThreadedFileReader(self.queue)
+        self.thread = ThreadedFileReader(self.frames_list)
         self.thread.start()
         self.periodic_call()
 
@@ -85,32 +131,31 @@ class GUI(Tk):
             self.after(100, self.periodic_call)
 
     def display_first_frame(self):
-        if self.queue.qsize():
-            try:
-                image = cv2.cvtColor(self.queue.get(), cv2.COLOR_BGR2RGB)
-                self.main_frame.configure(height=image.shape[0], width=image.shape[1])
-                self.main_frame_image = ImageTk.PhotoImage(Image.fromarray(image))
-                self.main_frame.configure(image=self.main_frame_image)
-            except queue.Empty:
-                pass
+        if len(self.frames_list) > 0:
+            self.buttons_frame.grid(row=2)
+            self.previous_button['state'] = DISABLED
+
+            image = cv2.cvtColor(self.frames_list[self.current_frame_index], cv2.COLOR_BGR2RGB)
+            self.main_frame.configure(height=image.shape[0], width=image.shape[1])
+            self.main_frame_image = ImageTk.PhotoImage(Image.fromarray(image))
+            self.main_frame.configure(image=self.main_frame_image)
 
 
 class ThreadedFileReader(threading.Thread):
-    def __init__(self, queue):
+    def __init__(self, frames):
         threading.Thread.__init__(self)
-        self.queue = queue
+        self.frames = frames
 
     def run(self):
         file_name = filedialog.askopenfilename(initialdir="/", title="Select a File",
                                                filetypes=[("video files", "*.mp4;*.avi")])
-
         cap = cv2.VideoCapture(file_name)
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         sample_rate = 1
         for frame_no in range(0, total_frames, sample_rate):
             cap.set(cv2.CAP_PROP_POS_FRAMES, frame_no)
             _, image = cap.read()
-            self.queue.put(image)
+            self.frames.append(image)
         print(file_name, total_frames)
 
 
